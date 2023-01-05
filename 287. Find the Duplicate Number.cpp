a71e110239ce0227892d761f5cc5616fc2e307fd
stack-queue-coder <60400251+stack-queue-coder@users.noreply.github.com>
2023-01-05 22:16:23 +0530

    Add files via upload

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // int n = nums.size();
+        // long long x = n*(n-1);
+        // x/=2;
+        // long long sum = 0;
+        // for(int i=0;i<n;i++){
+        //     sum += nums[i];
+        // }
+        // return sum - x;
+        int fast = 0;
+        int slow = 0;
+        while(1){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast){
+                break;
+            }
+        }
+        slow = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
